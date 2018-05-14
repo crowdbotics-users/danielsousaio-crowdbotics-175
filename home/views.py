6d8ecceb5cdeadbeaaf6d2d1f3c532dbee3f45dc
Saurav <sp.gharti@gmail.com>
2018-05-14 22:40:53 +0000

    add package django-shop

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ from home.models import CustomText, HomePage
 
 def home(request):
     packages = [
+	{'name':'django-shop', 'url': 'http://pypi.python.org/pypi/django-shop/0.11.3'},
 	{'name':'django-social-auth', 'url': 'http://pypi.python.org/pypi/django-social-auth/0.7.28'},
     ]
     context = {
